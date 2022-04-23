dca8ae5953ea78407e3a9b7728946bffa0f6c821
Filipe Rodrigo <filipevasconcelos91.rodrigo@gmail.com>
2022-04-23 00:53:16 -0300

    Finalization of Slider with jQuery

--- scripts/script.js
+++ scripts/script.js
@@ -4,27 +4,18 @@ $(function () {
     $('footer>.container').toggleClass('slider')
     $('div.menu').toggleClass('sliderMenu')
 
-    $('.logo').toggleClass('hide')
-    $('.avatar').toggleClass('hide')
     setTimeout(() => {
-      $('a>h4, .avatarDesc').toggleClass('hide')
+      $('.logo, .avatar, a>h4, .avatarDesc').toggleClass('hide')
     }, 240)
-    setTimeout(() => {
-      $('.aba>img').toggleClass('center')
-    }, 250)
   })
 
   $('.search').click(function () {
     $('aside').removeClass('slider')
     $('footer>.container').removeClass('slider')
     $('div.menu').removeClass('sliderMenu')
-    $('.logo').removeClass('hide')
-    $('.avatar').removeClass('hide')
+
     setTimeout(() => {
-      $('a>h4, .avatarDesc').removeClass('hide')
+      $('.logo, .avatar, a>h4, .avatarDesc').removeClass('hide')
     }, 240)
-    setTimeout(() => {
-      $('.aba>img').removeClass('center')
-    }, 250)
   })
 })
